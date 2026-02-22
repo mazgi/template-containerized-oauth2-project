aef9437fc01e8a8bdf10fada259cf9f2af652674
Hidenori "Hide" Matsuki <dev@mazgi.com>
2026-02-22 13:53:06 +0900

    remove all

--- e2e-tests/tests/index.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test'
-
-test('frontend has the stateus page', async ({ page }) => {
-  await page.goto(`${process.env.FRONTEND_URL}`)
-  await expect(page).toHaveTitle(/.*/)
-})
