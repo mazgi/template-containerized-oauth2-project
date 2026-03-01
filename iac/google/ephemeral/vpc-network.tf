293f519445665902d1b29f089a56ef643ab8e5a8
Hidenori "Hide" Matsuki <dev@mazgi.com>
2026-03-01 21:42:40 +0900

    shorten VPC Access Connector name to fit Google Cloud length limit

--- iac/google/ephemeral/vpc-network.tf
+++ iac/google/ephemeral/vpc-network.tf
@@ -1,6 +1,6 @@
 # VPC connector for Cloud Run -> VPC access
 resource "google_vpc_access_connector" "main" {
-  name   = "${var.app_unique_id}-connector"
+  name   = "${var.app_unique_id}-conn"
   region = local.gcp_region
 
   subnet {
