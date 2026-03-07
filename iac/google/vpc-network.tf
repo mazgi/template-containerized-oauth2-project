09c134695c33e6f78c48cff37ee4c2f7512f3d22
Hidenori "Hide" Matsuki <dev@mazgi.com>
2026-03-07 11:04:13 +0900

    fix dependency

--- iac/google/vpc-network.tf
+++ iac/google/vpc-network.tf
@@ -1,6 +1,8 @@
 resource "google_compute_network" "main" {
   name                    = "${var.app_unique_id}-vpc"
   auto_create_subnetworks = false
+
+  depends_on = [google_project_service.apis["compute.googleapis.com"]]
 }
 
 resource "google_compute_subnetwork" "main" {
