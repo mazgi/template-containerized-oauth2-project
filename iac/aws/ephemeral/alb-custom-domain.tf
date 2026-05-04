f325954a9434b213fa100c0ed9164316266a8958
Hidenori "Hide" Matsuki <dev@mazgi.com>
2026-05-04 13:19:46 +0900

    disable ACM

--- iac/aws/ephemeral/alb-custom-domain.tf
+++ iac/aws/ephemeral/alb-custom-domain.tf
@@ -16,10 +16,10 @@ data "awscc_ecs_express_gateway_service" "backend" {
 
 # --- Web service ---
 
-resource "aws_lb_listener_certificate" "web" {
-  listener_arn    = data.awscc_ecs_express_gateway_service.web.ecs_managed_resource_arns.ingress_path.listener_arn
-  certificate_arn = local.persistent.acm_wildcard_certificate_arn
-}
+# resource "aws_lb_listener_certificate" "web" {
+#   listener_arn    = data.awscc_ecs_express_gateway_service.web.ecs_managed_resource_arns.ingress_path.listener_arn
+#   certificate_arn = local.persistent.acm_wildcard_certificate_arn
+# }
 
 resource "aws_lb_listener_rule" "web_custom_domain" {
   listener_arn = data.awscc_ecs_express_gateway_service.web.ecs_managed_resource_arns.ingress_path.listener_arn
@@ -38,10 +38,10 @@ resource "aws_lb_listener_rule" "web_custom_domain" {
 
 # --- Backend service ---
 
-resource "aws_lb_listener_certificate" "backend" {
-  listener_arn    = data.awscc_ecs_express_gateway_service.backend.ecs_managed_resource_arns.ingress_path.listener_arn
-  certificate_arn = local.persistent.acm_wildcard_certificate_arn
-}
+# resource "aws_lb_listener_certificate" "backend" {
+#   listener_arn    = data.awscc_ecs_express_gateway_service.backend.ecs_managed_resource_arns.ingress_path.listener_arn
+#   certificate_arn = local.persistent.acm_wildcard_certificate_arn
+# }
 
 resource "aws_lb_listener_rule" "backend_custom_domain" {
   listener_arn = data.awscc_ecs_express_gateway_service.backend.ecs_managed_resource_arns.ingress_path.listener_arn
